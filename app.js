2b47afe87c953f4c99507b80915e350b82cdc1cb
Abdul Rehman <abdul.rehman132802@gmail.com>
2024-08-24 15:56:55 +0500

    commit

---
+++ app.js
@@ -0,0 +1,65 @@
+// Hello world
+console.log("Hello world")
+
+console.log(1,2,3)
+
+// variables
+let a=10
+console.log(a)
+
+b='obaid'
+B='Alam'
+console.log(b)
+console.log(b)
+
+console.log(B)
+
+const i=12
+console.log(i)
+
+// data types
+const v='Abdul Rehman' //string
+const w=20 //number
+const x=true //Boolean ,false , 0
+const d=undefined //undefined
+const q= null //null
+
+console.log(v)
+console.log(w)
+console.log(x)
+console.log(d)
+console.log(q)
+
+console.log(Boolean(true))
+console.log(false)
+console.log(Boolean(0))
+console.log(Boolean(1))
+console.log(Boolean(-4))
+console.log(Boolean(9))
+console.log(Boolean(5.98))
+
+// check type
+console.log(typeof(w))
+
+let wx=9
+console.log(typeof wx)
+
+const l=undefined
+console.log(typeof l)
+
+const r=null
+console.log(typeof r)
+
+// change data type
+
+p='3'
+const strtonum=Number(b)
+console.log(typeof strtonum)
+
+
+p='5c'
+const str_tonum=Number(b)
+console.log(typeof str_tonum)
+
+let something
+console.log(something)
